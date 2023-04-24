a03993dd9cb5088e64486bd5b16a45fb90c560db
hha2907 <ha.dh290702@gmail.com>
2023-04-24 10:26:58 +0300

    update SQL

--- server/db/003_db.sql
+++ server/db/003_db.sql
@@ -1,1 +1,9 @@
 INSERT INTO users (name, email) VALUES ('Mesi', 'Mesi@email.com');
+
+UPDATE users
+SET name = 'Dang Hoang Ha', email = 'ha.dh290702@gmail.com'
+WHERE name = 'Mesi' AND email = 'Mesi@email.com';
+
+UPDATE users
+SET name_tag = 'hha.2907'
+WHERE name = 'Dang Hoang Ha';
