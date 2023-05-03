59c21097e4afe70b19af7db8dbe82c1abf4df372
Carlos Reyes Muñoz <carlos.reyes.m@outlook.com>
2023-05-03 08:20:37 +0300

    remove query from sql where no name_tag column have been created yet

--- server/db/003_db.sql
+++ server/db/003_db.sql
@@ -3,7 +3,3 @@ INSERT INTO users (name, email) VALUES ('Mesi', 'Mesi@email.com');
 UPDATE users
 SET name = 'Dang Hoang Ha', email = 'ha.dh290702@gmail.com'
 WHERE name = 'Mesi' AND email = 'Mesi@email.com';
-
-UPDATE users
-SET name_tag = 'hha.2907'
-WHERE name = 'Dang Hoang Ha';
