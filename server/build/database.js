b3781db3d360eea1ec68959e8c00bf658150905b
hha2907 <ha.dh290702@gmail.com>
2023-05-09 20:57:48 +0300

    update

--- server/build/database.js
+++ server/build/database.js
@@ -4,11 +4,12 @@ exports.openDb = void 0;
 const pg_1 = require("pg");
 const openDb = () => {
     const pool = new pg_1.Pool({
-        user: "postgres",
-        host: "localhost",
-        database: "twitter",
-        password: "postgrespw",
+        user: "root",
+        host: "dpg-chd7i3l269vdj69mnm0g-a.frankfurt-postgres.render.com",
+        database: "twitter__55fz",
+        password: "93eM0eFg6fAKEmLwx7ySmUyCUVnvxJfZ",
         port: 5432,
+        ssl: true
     });
     return pool;
 };
--- server/src/database.ts
+++ server/src/database.ts
@@ -2,11 +2,12 @@ import { Pool } from "pg";
 
 export const openDb = (): Pool => {
   const pool: Pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "twitter",
-    password: "postgrespw",
+    user: "root",
+    host: "dpg-chd7i3l269vdj69mnm0g-a.frankfurt-postgres.render.com",
+    database: "twitter__55fz",
+    password: "93eM0eFg6fAKEmLwx7ySmUyCUVnvxJfZ",
     port: 5432,
+    ssl: true
   });
   return pool;
 };
